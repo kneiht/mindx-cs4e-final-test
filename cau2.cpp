817daf0df28c5f7cbbafacf2a24df5408f4595bb
Thien Nguyen <minhthien.k@gmail.com>
2025-11-04 19:07:18 +0700

    feat(problem-2): create class Nguoi

--- cau2.cpp
+++ cau2.cpp
@@ -9,20 +9,21 @@ private:
     long giave;
     
 public:
-    // Hàm tạo
+    // Hàm tạo mặc định
     Vemaybay() {
         tenchuyen = "";
         ngaybay = "";
         giave = 0;
     }
     
+    // Hàm tạo có tham số
     Vemaybay(string ten, string ngay, long gia) {
         tenchuyen = ten;
         ngaybay = ngay;
         giave = gia;
     }
     
-    // Hàm hủy
+    // Hàm hủy 
     ~Vemaybay() {}
     
     // Hàm nhập
@@ -48,9 +49,58 @@ public:
     }
 };
 
+
+// Lớp Nguoi
+class Nguoi {
+protected:
+    string hoten;
+    string gioitinh;
+    int tuoi;
+    
+public:
+    // Hàm tạo mặc định
+    Nguoi() {
+        hoten = "";
+        gioitinh = "";
+        tuoi = 0;
+    }
+    
+    // Hàm tạo có tham số
+    Nguoi(string ten, string gt, int t) {
+        hoten = ten;
+        gioitinh = gt;
+        tuoi = t;
+    }
+    
+    // Hàm hủy
+    ~Nguoi() {}
+    
+    // Hàm nhập
+    void Nhap() {
+        cout << "Nhap ho ten: ";
+        cin.ignore();
+        getline(cin, hoten);
+        cout << "Nhap gioi tinh: ";
+        getline(cin, gioitinh);
+        cout << "Nhap tuoi: ";
+        cin >> tuoi;
+    }
+    
+    // Hàm xuất
+    void Xuat() {
+        cout << "Ho ten: " << hoten << ", Gioi tinh: " << gioitinh << ", Tuoi: " << tuoi << endl;
+    }
+};
+    
+
 int main() {
     Vemaybay ve;
     ve.Nhap();
     ve.Xuat();
+
+    Nguoi nguoi;
+    nguoi.Nhap();
+    nguoi.Xuat();
     return 0;
+    
 }
